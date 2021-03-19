08e5ad781aa291c5794b57f6e7dc0768c54291b0
Pavel Tobias <pavel.tobias@flowup.cz>
2021-03-19 15:13:51 +0100

    Tasks 08--09

--- currency-convertor/src/main/java/cz/muni/fi/pa165/MainJavaConfig.java
+++ currency-convertor/src/main/java/cz/muni/fi/pa165/MainJavaConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import static cz.muni.fi.pa165.DependencyInjectionTest.testDi;
 
 public class MainJavaConfig {
-    private static final String BASE_PACKAGE = "cz.muni.fi.pa165.currency";
+    private static final String BASE_PACKAGE = "cz.muni.fi.pa165";
 
     public static void main(String[] argv) {
         testDi(new AnnotationConfigApplicationContext(BASE_PACKAGE));
